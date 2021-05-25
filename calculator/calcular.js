ac07d43a9c252c6a936236bc369b81d54dba0180
Rodrigo Gonçalves Cambraia Soares <rodrigocambraia.jr@gmail.com>
2021-05-25 20:58:27 -0300

    Calculator JS

--- calculator/calcular.js
+++ calculator/calcular.js
@@ -1,26 +1,26 @@
 function insert(num){
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+    var numero = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = numero + num;
 }
 
 function clean(){
-    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('resultado').value = "";
 }
 
 function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+    var resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado.substring(0, resultado.length - 1);
 }
 
 function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
+    var resultado = document.getElementById('resultado').value;
 
     if(resultado)
     {
-        document.getElementById('resultado').innerHTML = eval(resultado);
+        document.getElementById('resultado').value = eval(resultado);
     }
     else
     {
-        document.getElementById('resultado').innerHTML = "";
-    }
+        document.getElementById('resultado').value = "";
+    }        
 }
